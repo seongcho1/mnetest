f4595288dbad82be4273e6870b2ea421c911f1a2
seongcho <indivisibilibus@gmail.com>
2023-01-13 11:29:32 +0900

    data

---
+++ Loading.py
@@ -0,0 +1,12 @@
+import numpy as np
+import mne
+
+sample_data_folder = mne.datasets.sample.data_path('~/goinfre/test/mnetest/data')
+print(sample_data_folder)
+
+# sample_data_raw_file = (sample_data_folder / 'MEG' / 'sample' /
+#                         'sample_audvis_filt-0-40_raw.fif')
+# raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+# print(raw)
+# print(raw.info)
