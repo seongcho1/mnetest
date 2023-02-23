a296af4ba82c347fd3c14477ee2d415cdbb62cd3
seongcho <indivisibilibus@gmail.com>
2023-02-23 17:37:00 +0900

    conda

---
+++ check_conda.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+conda info --envs
+#conda activate 42AI-$USER
+which python
+python -V
+python -c "print('Hello World!')"
+
+#pip list --format=freeze > requirements.txt
+#pip install -r requirements.txt
